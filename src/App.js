86b7413cd2667c1e72102c94efba38b557dcfd87
= <shanekearney91@gmail.com>
2023-06-15 23:29:34 +0100

    Added functionality to create data driven form input elements and to map them to the layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,80 @@
+import { useState } from "react";
 import "./App.css";
+import FormInput from "./components/FormInput";
 
 function App() {
-  return <div className="App"></div>;
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    fullname: "",
+    birthday: "",
+    password: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username",
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      label: "Email",
+    },
+    {
+      id: 3,
+      name: "fullname",
+      type: "text",
+      placeholder: "Full Name",
+      label: "Full Name",
+    },
+    {
+      id: 4,
+      name: "birthday",
+      type: "text",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 5,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      label: "Password",
+    },
+  ];
+  function handleSubmit(e) {
+    e.preventDefault();
+    // const data = new FormData(e.target);
+    // console.log(Object.fromEntries(data.entries()));
+  }
+
+  function handleChange(e) {
+    console.log(e.target.name);
+    setValues({ ...values, [e.target.name]: e.target.value });
+  }
+
+  console.log(values);
+  return (
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={handleChange}
+          />
+        ))}
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
