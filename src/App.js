f08ac8fe08151538a9f51140bd832a1211b08b8e
= <shanekearney91@gmail.com>
2023-06-19 22:45:28 +0100

    added pattern key values to inputs array of objects

--- src/App.js
+++ src/App.js
@@ -58,6 +58,7 @@ function App() {
       errorMessage:
         "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
       label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
       required: true,
     },
     {
@@ -67,6 +68,7 @@ function App() {
       placeholder: "Confirm Password",
       errorMessage: "Passwords dont't match!",
       label: "Confirm Password",
+      pattern: values.password,
       required: true,
     },
   ];
