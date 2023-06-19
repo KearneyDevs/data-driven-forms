f397fba935cc51a689be0d30dcacba8f9229c653
= <shanekearney91@gmail.com>
2023-06-19 23:04:06 +0100

    Moved state and data out of main component and created export variables to import them

--- src/App.js
+++ src/App.js
@@ -1,78 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import { inputs, inputState } from "./data/inputData";
 import FormInput from "./components/FormInput";
 
 function App() {
-  const [values, setValues] = useState({
-    username: "",
-    email: "",
-    fullname: "",
-    birthday: "",
-    password: "",
-    confirmPassword: "",
-  });
+  const [values, setValues] = useState(inputState);
 
-  const inputs = [
-    {
-      id: 1,
-      name: "username",
-      type: "text",
-      placeholder: "Username",
-      errorMessage:
-        "Username should be 3-16 characters and shouldn't include any special characters!",
-      label: "Username",
-      pattern: "^[A-Za-z0-9]{3,16}$",
-      required: true,
-    },
-    {
-      id: 2,
-      name: "email",
-      type: "email",
-      placeholder: "Email",
-      errorMessage: "It should be a valid email address!",
-      label: "Email",
-      required: true,
-    },
-    {
-      id: 3,
-      name: "fullname",
-      type: "text",
-      placeholder: "Full Name",
-      errorMessage:
-        "Username should be 3-16 characters and shouldn't include any special characters!",
-      label: "Full Name",
-      required: true,
-    },
-    {
-      id: 4,
-      name: "birthday",
-      type: "date",
-      placeholder: "Birthday",
-      errorMessage: "",
-      label: "Birthday",
-    },
-    {
-      id: 5,
-      name: "password",
-      type: "password",
-      placeholder: "Password",
-      errorMessage:
-        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
-      label: "Password",
-      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
-      required: true,
-    },
-    {
-      id: 6,
-      name: "confirmPassword",
-      type: "password",
-      placeholder: "Confirm Password",
-      errorMessage: "Passwords dont't match!",
-      label: "Confirm Password",
-      pattern: values.password,
-      required: true,
-    },
-  ];
   function handleSubmit(e) {
     e.preventDefault();
     const data = new FormData(e.target);
