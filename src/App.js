b9a7a460dfc36bdc3ee7c282a3fcdaff7fba4282
= <shanekearney91@gmail.com>
2023-06-19 22:24:51 +0100

    Added additional values to input array of objects for required fields and patterns using regex for the username field

--- src/App.js
+++ src/App.js
@@ -17,27 +17,37 @@ function App() {
       name: "username",
       type: "text",
       placeholder: "Username",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special characters!",
       label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
     },
     {
       id: 2,
       name: "email",
       type: "email",
       placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
       label: "Email",
+      required: true,
     },
     {
       id: 3,
       name: "fullname",
       type: "text",
       placeholder: "Full Name",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special characters!",
       label: "Full Name",
+      required: true,
     },
     {
       id: 4,
       name: "birthday",
-      type: "text",
+      type: "date",
       placeholder: "Birthday",
+      errorMessage: "",
       label: "Birthday",
     },
     {
@@ -45,7 +55,19 @@ function App() {
       name: "password",
       type: "password",
       placeholder: "Password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
       label: "Password",
+      required: true,
+    },
+    {
+      id: 6,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      errorMessage: "Passwords dont't match!",
+      label: "Confirm Password",
+      required: true,
     },
   ];
   function handleSubmit(e) {
