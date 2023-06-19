fea5696b5044ad0b48d31ea6932ff81b997eee35
= <shanekearney91@gmail.com>
2023-06-19 22:11:46 +0100

    removed console.log and added a h1 element to the form

--- src/App.js
+++ src/App.js
@@ -58,10 +58,10 @@ function App() {
     setValues({ ...values, [e.target.name]: e.target.value });
   }
 
-  console.log(values);
   return (
     <div className="app">
       <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
         {inputs.map((input) => (
           <FormInput
             key={input.id}
